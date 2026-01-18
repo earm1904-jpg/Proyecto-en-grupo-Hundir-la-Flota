00d0fea2e42260fcfaac84932051046ccceddc40
Edwin Ramírez <earm1904@gmail.com>
2026-01-18 18:08:33 +0000

    Se crearon las naves del PC y ubicación de las mismas

--- code/menu.c
+++ code/menu.c
@@ -38,8 +38,7 @@ int main()
     printf("Debes ubicar 6 anves:\nUn Portaviones de cuatro posiciones,\nDos Cruceros de tres posiciones cada uno\ny Tres Patrulleros de dos posiciones cada uno.\nEl tablero cuenta con numeros que van del 0 al 11.\nCon estos numeros te puedes guiar para dar la ubicacion de tus naves\n\n");
     printf("Recuerda que los barcos tiene cuatro direcciones principales:\n LA PROA (Delante)\n LA POPA (Atrás)\n BABOR (Izquierda)\n ESTRIBOR (Derecha) \n\n");
     ShowTablero(TabPlayer);
-    //Crando las naves y asignado nombarco y tamNave;
-
+    //Creando las naves Player y asignado nombarco y tamNave;
     Naves Portaviones={'P',4};
     Naves Crucero1={'C',3};
     Naves Crucero2={'C',3};
@@ -47,13 +46,31 @@ int main()
     Naves Pratullero2={'T',2};
     Naves Pratullero3={'T',2};
 
+    //Creando las naves PC y asignado nombarco y tamNave;
+    Naves PortavionesPC={'P',4};
+    Naves Crucero1PC={'C',3};
+    Naves Crucero2PC={'C',3};
+    Naves Pratullero1PC={'T',2};
+    Naves Pratullero2PC={'T',2};
+    Naves Pratullero3PC={'T',2};
+
     //Ubicando cada nave del Player en el tablero TabPlayer
+    UbicarNavesPlayer(&TabPlayer,&Portaviones);
     UbicarNavesPlayer(&TabPlayer,&Crucero1);
     UbicarNavesPlayer(&TabPlayer,&Crucero2);
-    UbicarNavesPlayer(&TabPlayer,&Portaviones);
     UbicarNavesPlayer(&TabPlayer,&Pratullero1);
     UbicarNavesPlayer(&TabPlayer,&Pratullero2);
     UbicarNavesPlayer(&TabPlayer,&Pratullero3);
+
+    //Ubicando cada nave del PC en el tablero  TabContr
+    UbicarNavesPC(&TabContr, &PortavionesPC);
+    UbicarNavesPC(&TabContr, &Crucero1PC);
+    UbicarNavesPC(&TabContr, &Crucero2PC);
+    UbicarNavesPC(&TabContr, &Pratullero1PC);
+    UbicarNavesPC(&TabContr, &Pratullero2PC);
+    UbicarNavesPC(&TabContr, &Pratullero3PC);
+    
+    
     
     break;
 
