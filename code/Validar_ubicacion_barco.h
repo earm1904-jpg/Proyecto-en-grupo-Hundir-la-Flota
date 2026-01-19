db1de3f472b168b9f18930cfdb2f5feaf1ab9b59
Edwin Ramírez <earm1904@gmail.com>
2026-01-19 01:24:59 +0000

    Commit 2

--- code/Validar_ubicacion_barco.h
+++ code/Validar_ubicacion_barco.h
@@ -1,7 +1,7 @@
 #ifndef VALIDAR_UBICACION_BARCO_H
 #define VALIDAR_UBICACION_BARCO_H
 #include "juego.h"
-
+//Commit 2
 int FueradelMapa(Naves *Nav);
 int BarcoAdyacente(tablero *tabJu_Or, Naves *NavPL_OR);
 void UbicarBarco(tablero *tabJu_Or, Naves *NavPL_OR);
