0843e2cf4426c1238f52aa88c8825941acef4c18
Pocho019 <pochopro2008@gmail.com>
2026-01-19 16:24:22 +0000

    añadi mas funciones y parametros para que funcione turnos.c

--- code/turno.h
+++ code/turno.h
@@ -6,4 +6,7 @@ extern int CoorImpFilas, CoorImpColumn, turno;
 
 int ImpactoNav(Nave *Nav, tablero *tab, tablero *tabDisp);
 void turnosPlayers(Nave *Nav, tablero *tabPlayer, tablero *tabPC);
+int AtaquePC(tablero *tabPlayer, tablero *tabDisparosPC);
+int ContarBarcosRestantes(tablero *tab);
+
 #endif
