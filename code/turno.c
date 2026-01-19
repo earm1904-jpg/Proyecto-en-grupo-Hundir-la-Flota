42d9ba3f6d08a6527335b7eec909845f186b4857
PabloGiner99 <pabloginer9926@gmail.com>
2026-01-19 14:27:13 +0000

    Alternativa a los turnos sin bucle for

--- code/turno.c
+++ code/turno.c
@@ -72,3 +72,76 @@ void turnosPlayers(Nave *Nav, tablero *tabPlayer, tablero *tabPC){
 
 
 }
+/*//Devuelve 1 si impactas en un barco y 0 si has fallado o ya habias dicho las coordenadas.
+
+int ImpactoNav(Nave *Nav, tablero *tab, tablero *tabDisp){
+    printf("Dime las coordenadas de disparo.\n", TAM_TABLERO -1);
+    printf("Dime la coordenada de la fila: ");
+
+//Los dos primeros if es para comprobar que lo que se introduce son números y no letras o símbolos
+    if (scanf("%d", &CoorImpFilas)!= 1)
+        return 0;
+    
+    printf("Dime la coordenada de la columna: ");
+    if (scanf("%d", &CoorImpColumn)!= 1)
+        return 0;
+// Este if es para comprobar que las coordenadas están dentro del tablero.
+    if (CoorImpFilas < 0 || CoorImpFilas >= TAM_TABLERO || CoorImpColumn < 0 || CoorImpColumn >= TAM_TABLERO){
+        printf ("Coordenadas no válidas\n");
+        return 0;
+    }
+//Este if es para ver si ya habías introducido estas coordenadas.
+    if (tabDisp->espacio[CoorImpFilas][CoorImpColumn]== IMPACTO || tabDisp->espacio[CoorImpFilas][CoorImpColumn] == FALLA)
+    {
+        printf("Ya has disparado en esta posición. \n");
+        return 0;
+    }
+//Como no hace falta recorrer todo el tablero, vamos a hacer una comprobación directa en la coordenada puesta
+    if (tab->espacio[CoorImpFilas][CoorImpColumn]!= AGUA)
+    {
+        if (Nav != NULL && Nav->ImpacNav <= 0)
+        {
+            Nav->ImpacNav = Nav->tamNave;
+        }
+            
+        if (Nav != NULL)
+        {
+            Nav->ImpacNav--;
+        }
+
+        tab->espacio[CoorImpFilas][CoorImpColumn] = IMPACTO;
+        tabDisp->espacio[CoorImpFilas][CoorImpColumn] = IMPACTO;
+
+        if (Nav != NULL && Nav->ImpacNav == 0)
+        {
+            hundidos++;
+            printf("Has hundido un barco. \n");
+        }
+        else
+        {
+            printf("Has impoactado un barco. \n");
+        }
+        return 1; 
+        //Devuelve uno porque ha sido un acierto
+    }
+    tabDisp->espacio[CoorImpFilas][CoorImpColumn] = FALLA;
+    printf("No le has dado a ningún barco. \n");
+    return 0;
+}
+//Funcion para contar los turnos.
+void turnosPlayers(Nave *Nav, tablero *tabPlayer, tablero *tabPC){
+    int acierto = ImpactoNav(Nav, tabPC, tabPlayer);
+    disparos++;
+    //Si aciertas repites
+    if (acierto)
+    {
+        acierto = ImpactoNav(Nav, tabPC, tabPlayer);
+        disparos++;
+    }
+    //si fallas cambias turno.
+    else
+    {
+        turno++;
+    }
+    
+}*/
