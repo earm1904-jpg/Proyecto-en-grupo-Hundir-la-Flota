997f4f22d49841cdc4b3e125c6226971b5139327
Edwin Ramírez <earm1904@gmail.com>
2026-01-19 00:18:26 +0000

    incluyeron las funciones y variables

--- code/turno.h
+++ code/turno.h
@@ -1,4 +1,9 @@
 #ifndef TURNO_H
 #define TURNO_H
+#include "juego.h"
 
+int CoorImpFilas,CoorImpColumn,turno;
+
+int ImpactoNav(Nave *Nav, tablero *tab, tablero *tabDisp);
+void turnosPlayers(Nave *Nav, tablero *tabPlayer, tablero *tabPC);
 #endif
